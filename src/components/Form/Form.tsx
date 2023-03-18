bb2c9bd43c7862db765de28460898768389587a0
pskrzypek97 <tofanek9243@gmail.com>
2023-03-18 11:02:34 +0100

    Added styles for inputs and button

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,13 +1,23 @@
+import Dropdown from '../Dropdown/Dropdown';
+
 import styles from '../../styles/form.module.css';
 
 const Form = () => {
 	return (
 		<form action="#" className={styles.form}>
 			<div className={styles['form-group']}>
-				<input type="text" id="name" name="name" />
+				<input
+					type="text"
+					id="name"
+					name="name"
+					className={styles['input-error']}
+				/>
 				<label htmlFor="name" className={styles['form-label']}>
 					Name
 				</label>
+				<span className={styles['error-span']}>
+					djioaidaidjiajdiajdpajdpajdjadjpa
+				</span>
 			</div>
 			<div className={styles['form-group']}>
 				<input type="text" id="preparation-time" name="preparation-time" />
@@ -15,6 +25,28 @@ const Form = () => {
 					Preparation Time
 				</label>
 			</div>
+
+			<div className={styles['form-group']}>
+				<Dropdown />
+			</div>
+
+			<div className={styles['form-group']}>
+				<input type="number" id="no_of_slices" name="no_of_slices" />
+				<label htmlFor="no_of_slices" className={styles['form-label']}>
+					Number of slices
+				</label>
+			</div>
+
+			<div className={styles['form-group']}>
+				<input type="number" id="diameter" name="diameter" />
+				<label htmlFor="diameter" className={styles['form-label']}>
+					Diameter
+				</label>
+			</div>
+
+			<button type="submit" className={styles['form__button']}>
+				Submit
+			</button>
 		</form>
 	);
 };
